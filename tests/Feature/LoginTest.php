07f733bf49afbcbd6d93a036d304f39f1c65ac37
chikamasuda <c.m.062820@gmail.com>
2023-06-21 04:57:36 +0900

    ログインのテストを実装

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Artisan;
+
+class LoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // ユーザーの作成
+        User::factory()->create([
+            'password' => Hash::make('password')
+        ]);
+
+        Artisan::call('passport:install');
+    }
+
+    /**
+     * テスト成功
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $user = User::first();
+
+        $user_data = [
+            'email' => $user->email,
+            'password' => 'password',
+        ];
+
+        $response = $this->post('/api/users/login', $user_data);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            "token"
+        ]);
+    }
+
+    /**
+     * テスト失敗
+     *
+     * @return void
+     */
+    public function testFailedLogin()
+    {
+        //正しくないユーザー情報
+        $user_data = [
+            'email' => 'sample@test.com',
+            'password' => 'incorrect_password',
+        ];
+
+        $response = $this->post('/api/users/login', $user_data);
+        $response->assertStatus(401);
+        $response->assertJson([
+            'message' => 'Unauthorized',
+        ]);
+    }
+
+    /**
+     * テスト失敗(バリデーションエラー)
+     *
+     * @return void
+     */
+    public function testFailedLoginValidation()
+    {
+        //空のユーザー情報
+        $user_data = [
+            'email' => '',
+            'password' => '',
+        ];
+
+        $response = $this->post('/api/users/login', $user_data);
+        $response->assertStatus(422);
+        $response->assertJson([
+            'data' => [
+                'data' => [],
+                'errors' => [
+                    'email' => ['Eメールは必ず指定してください。'],
+                    'password' => ['パスワードは必ず指定してください。']
+                ]
+            ],
+        ]);
+    }
+}
